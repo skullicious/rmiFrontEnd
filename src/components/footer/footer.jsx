603719e65a1d914f55c9b4223969297e88f6d841
Damien Sweeney <38982429+skullicious@users.noreply.github.com>
2020-07-02 15:11:58 +0100

    Restyles buttons

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -5,16 +5,25 @@ import BottomNavigation from "@material-ui/core/BottomNavigation";
 import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
 import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
 
+import Fab from "@material-ui/core/Fab";
+
 const useStyles = makeStyles({
   root: {
     position: "fixed",
-    left: "0",
     bottom: "0",
     width: "100%",
-    backgroundColor: "white",
-    textAlign: "center",
     zIndex: 3,
-    opacity: 0.8,
+    background: "transparent",
+  },
+  backChevron: {
+    position: "absolute",
+    top: "-20px",
+    left: "35px",
+  },
+  forwardChevron: {
+    position: "absolute",
+    top: "-20px",
+    right: "35px",
   },
 });
 
@@ -29,29 +38,25 @@ export default function Footer({ bottomNavClick, index, iconCount }) {
     <div>
       <BottomNavigation className={classes.root}>
         {index !== 0 && (
-          <Tab
-            icon={
-              <ArrowBackIosIcon
-                onClick={handleChange.bind(this, "backward")}
-                value="backward"
-              />
-            }
-          />
+          <Fab
+            onClick={handleChange.bind(this, "backward")}
+            value="backward"
+            className={classes.backChevron}
+            color="primary"
+          >
+            <ArrowBackIosIcon />
+          </Fab>
         )}
 
-        <Tab />
-        <Tab />
-        <Tab />
-
         {index !== iconCount - 1 && (
-          <Tab
-            icon={
-              <ArrowForwardIosIcon
-                onClick={handleChange.bind(this, "forward")}
-                value="forward"
-              />
-            }
-          />
+          <Fab
+            onClick={handleChange.bind(this, "forward")}
+            value="forward"
+            className={classes.forwardChevron}
+            color="primary"
+          >
+            <ArrowForwardIosIcon />
+          </Fab>
         )}
       </BottomNavigation>
     </div>
